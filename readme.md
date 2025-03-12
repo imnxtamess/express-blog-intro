4d34d590ac8dfc3661d7528ea7f3862813532497
imnxtamess <davide.morisco.work@gmail.com>
2025-03-12 14:21:32 +0100

    add route that returns posts in a json format

--- readme.md
+++ readme.md
@@ -4,7 +4,10 @@ Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con 
 Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
 
 Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+
 Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
+
 Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+
 Testare su postman
 Buon divertimento e confermate lettura come al solito :baby-yoda:!
--- server.js
+++ server.js
@@ -12,3 +12,47 @@ app.get("/", () => {
 
   res.send("Server del mio blog")
 })
+
+app.get("/api/v1/posts", (res, req) => {
+
+  const posts = [
+    {
+      id: 1,
+      title: "post 1",
+      body: "lorem ipsum",
+      img: "./images/img1.jpg",
+      tags: ["lorem", "picsum", "ipsum", "dorime", "adapare"]
+    },
+    {
+      id: 2,
+      title: "post 2",
+      body: "lorem ipsum",
+      img: "./images/img2.jpg",
+      tags: ["lorem", "picsum", "ipsum", "dorime", "adapare"]
+    },
+    {
+      id: 3,
+      title: "post 3",
+      body: "lorem ipsum",
+      img: "./images/img3.jpg",
+      tags: ["lorem", "picsum", "ipsum", "dorime", "adapare"]
+    },
+    {
+      id: 4,
+      title: "post 4",
+      body: "lorem ipsum",
+      img: "./images/img4.jpg",
+      tags: ["lorem", "picsum", "ipsum", "dorime", "adapare"]
+    },
+    {
+      id: 5,
+      title: "post 5",
+      body: "lorem ipsum",
+      img: "./images/img5.jpg",
+      tags: ["lorem", "picsum", "ipsum", "dorime", "adapare"]
+    },
+  ]
+
+  res.json(posts)
+
+})
